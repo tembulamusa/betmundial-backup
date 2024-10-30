88c21c3d4f2065f46674b301f46136328175ffab
tembulamusa <woesites@gmail.com>
2024-10-30 17:27:03 +0300

    instantiate surecoin

---
+++ src/components/pages/sure-coin/user-choices.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import { Context } from "../../../context/store";
+
+const SureCoinChoices = (props) => {
+    const [state, dispatch] = useContext(Context);
+    
+    return (
+        <>
+            {}
+        </>
+    )
+}
+
+export default React.memo(SureCoinChoices);
