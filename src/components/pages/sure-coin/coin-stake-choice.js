015e78df92736f6d84edb38fcb83acd70dc68cc2
Moses tembula <jameswangabivan@gmail.com>
2024-11-12 21:21:22 +0300

    feat: half-add-surecoin for paul

--- src/components/pages/sure-coin/coin-stake-choice.js
+++ src/components/pages/sure-coin/coin-stake-choice.js
@@ -7,10 +7,15 @@ import { CgAdd, CgRemove } from "react-icons/cg";
 
 const CoinStakeChoice = (props) => {
     const {coinnumber, isspinning, istakingbets} = props;
-    const [amount, setAmount] = useState();
+    const [amount, setAmount] = useState(10);
     const [choice, setChoice] = useState("head");
     const [state, dispatch] = useContext(Context);
-    const [inputErrors, setInputErrors] = useState({})
+    const [inputErrors, setInputErrors] = useState({});
+    const [disabledBetBtn, setDisabledBetBtn] = useState(false);
+    const [defaultAmountChange, setDefaultAmountChange] = useState(10);
+    const [minimumBetAmount, setMinimumAmount] = useState(5);
+    const [netWinning, setNetWinning] = useState();
+    const [pickedBtn, setPickedBtn] = useState(false);
 
     useEffect(() => {
         
@@ -29,6 +34,31 @@ const CoinStakeChoice = (props) => {
         // else show errors
         setAmount(e.value);
     }
+
+    const changeAmount = (changeType) => {
+
+        if (changeType === "increase") {
+            let newAmount = defaultAmountChange + amount
+            setAmount(newAmount);
+        } else if (changeType === "decrease") {
+            let newAmount = amount - defaultAmountChange
+            if (newAmount > minimumBetAmount) {
+                setAmount(newAmount);
+            } else {
+                setAmount(minimumBetAmount)
+            }
+        }
+    }
+
+    const pickClick = (pick) => {
+
+        if (pick === "tails") {
+            if (pickedBtn === "tails") {
+
+            }
+        }
+
+    }
     return (
         <>
             <div className="user-input-section" onClick={() => setcanplayTheitems()}>
@@ -36,26 +66,49 @@ const CoinStakeChoice = (props) => {
                 <div className="auto-bet-selector">
                     auto selector
                 </div>
-                <div className="user-input-main">
-                    <div className="input-collector">
-                        <div className="amount">
-                            <CgRemove />
-                            
+                <div className="user-input-main flex">
+                    <div className="input-collector flex-col w-1/2">
+                        <div className="sure-coin-amount-input-section flex font-[700]">
+                            <CgRemove
+                                onClick={() => changeAmount("decrease") }
+                                className="mt-1 text-3xl opacity-60 hover:opacity-100 cursor-pointer" />
                             <input
                                 onChange={(e) => amountChanged(e)} 
                                 type="number"
                                 value={amount}
-                                min={2}
-                                className="border border-gray-200 user-amount-input"/>
+                                min={minimumBetAmount}
+                                className="border-[transparent] bg-[transparent] user-amount-input px-3"/>
 
-                            <CgAdd className=""/>
+                            <CgAdd className="mt-1 text-3xl opacity-60 hover:opacity-100 cursor-pointer" onClick={() => changeAmount("increase") }/>
                         </div>
-                        <div>touch Coin to change</div>
+                        <div className="win-info">
+                            <div className="odds"><span className="multiplier-txt">Odds</span> <span className="multiplier-value float-end">X2</span></div>
+                            <div className="win-amount"><span>Payout</span><span className="float-end">{amount * 2}</span></div>
+                        </div>
                     </div>
-                    <button className="input-place-bet-btn ">
-                        <div>Bet</div>
-                        <div>{}</div>
-                    </button>
+                    <div className="flex-col w-1/2">
+                        <div className={`input-place-bet-btn ml-4 text-center w-full`}>
+
+                            <div className="">
+                                <button
+                                    className={`pickBtn head uppercase ${pickedBtn === "heads" ? "selected-btn selected-head" : ""}`}
+                                    onClick={() => pickClick("heads")}
+                                    disabled={disabledBetBtn}>
+                                        Heads
+                                </button>
+                            </div>
+
+                            <div className="">
+                                <button
+                                    className={`pickBtn tail uppercase ${pickedBtn === "tails" ? "selected-btn selected-tail" : ""}`} 
+                                    onClick={() => pickClick("tails")}
+                                    disabled={disabledBetBtn}>
+                                    Tails
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
                 <div className="autopick-settings">autopick-settings</div>
             </div>
--- src/components/pages/sure-coin/index.js
+++ src/components/pages/sure-coin/index.js
@@ -2,10 +2,12 @@ import React, { useContext, useEffect, useState } from "react";
 import RotatingCoin from "./rotating-coin";
 import CoinStakeChoice from "./coin-stake-choice";
 import { Context } from "../../../context/store";
+import { FaCog, FaComments, FaInfo } from "react-icons/fa";
+import { getFromLocalStorage } from "../../utils/local-storage";
 
 const SureCoinIndex = (props) => {
     const [state, dispatch] = useContext(Context);
-    const [userCoinCount, setUserCoinCount] = useState(2);
+    const [userCoinCount, setUserCoinCount] = useState(1);
     const [userChoices, setUserChoices] = useState({});
     const [timeToStart, setTimeToStart] = useState(5);
     const [timeToEnd, setTimeToEnd] = useState(10);
@@ -14,6 +16,7 @@ const SureCoinIndex = (props) => {
     const [isSpinning, setIsSpinning] = useState(false);
     const [preparingToSpin, setPreparingToSpin] = useState(false);
     const [dummyTriggerCounter, setDummyTriggerCounter] = useState(0);
+    const [userBal, setUserBal] = useState(0.00);
 
 
     // Dummy trigger
@@ -41,15 +44,20 @@ const SureCoinIndex = (props) => {
 
     useEffect(() => {
         dispatch({type:"SET", key:"surecoinlaunched", payload:true})
+        let userBalance = getFromLocalStorage("user");
+        if(userBalance && userBalance.balance) {
+            setUserBal(userBalance.balance);
+        }
         return () => {
             dispatch({type:"DEL", key: "surecoinlaunched"})
         }
+
     }, [])
     
     const PageHeader = (props) => {
 
         return (
-            <h4 className="text-centr col-md-12 bg-primary p-2 text-enter justify-content-between">
+            <h4 className="text-centr col-md-12 text-enter justify-content-between">
                 Surecoin
             </h4>
         )
@@ -58,15 +66,18 @@ const SureCoinIndex = (props) => {
         <div className="launched-sure-coin">
             <PageHeader />
             <div className="surecoin-body">
-                <div className="surecoin-main flex">
-                    <div className={`sure-coin-betting-section flex-col  w-8/12`}>
-                        <div className="sure-coin-header">
-                            <div className="">
-                                SureCoin
+                <div className="surecoin-main md:flex">
+                    <div className={`sure-coin-betting-section md:flex-col  w-full md:w-8/12`}>
+                        <div className="sure-coin-header row">
+                            <div className="col-4">
+                                SureCoin <span className=""><FaInfo className="inline-block md:hidden"/><button className="hidden md:inline-block basic-highlight-alert ml-3 font-[300] bg-[#f5a623] text-[#5f3816] rounded-md px-3">How to play</button></span>
                             </div>
-
-                            <div className="">
-                                
+                            <div className="col-8">
+                                <div className="float-end flex">
+                                    <div className="inline-block">{userBal} KES</div>
+                                    <div className="border-l border-gray-100 ml-2 pl-2"><FaCog className="inline-block"/></div>
+                                    <div className="border-l border-gray-100 ml-2 pl-2 "><FaComments className="inline-block"/></div>
+                                </div>
                             </div>
                         </div>
                         <div className="casino-service-sure-coin">
@@ -92,7 +103,7 @@ const SureCoinIndex = (props) => {
                         </div>
                     </div>
                     
-                    <div className={`comments-settings flex-col w-4/12`}>
+                    <div className={`comments-settings md:flex-col md:w-4/12 w-full`}>
                         tabs <br/>
                         changes to comments and settings alternatively
                     </div>
