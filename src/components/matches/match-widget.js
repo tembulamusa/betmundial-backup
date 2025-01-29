99e28975f3070b5d7c457433b690a81876a82d6e
Moses tembula <jameswangabivan@gmail.com>
2025-01-29 16:45:22 +0300

    ff

--- src/components/matches/match-widget.js
+++ src/components/matches/match-widget.js
@@ -4,38 +4,32 @@ import '../../assets/css/theme.css';
 const MatchWidget = (props) => {
     const {parentMatchId} = props;
     useEffect(() => {
-    // Dynamically load the script
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = "https://widgets.sir.sportradar.com/507f748510ff83b1f73738bd3df69714/widgetloader";
-    script.setAttribute("n", "SIR");
-
-    script.onload = () => {
-        // Initialize the widget after the script has loaded
+        
+        (function(a,b,c,d,e,f,g,h,i){a[e]||(i=a[e]=function(){(a[e].q=a[e].q||[]).push(arguments)},i.l=1*new Date,i.o=f,
+            g=b.createElement(c),h=b.getElementsByTagName(c)[0],g.async=1,g.src=d,g.setAttribute("n",e),h.parentNode.insertBefore(g,h)
+            )})(window,document,"script", "https://widgets.sir.sportradar.com/507f748510ff83b1f73738bd3df69714/widgetloader", "SIR", {
+                theme: false, // using custom theme
+                language: "en"
+            });
         if (window.SIR) {
-        window.SIR("addWidget", ".sr-widget-1", "match.lmtPlus", {
-            streamToggle: "onPitchButton",
-            layout: "double",
-            detailedScoreboard: "disable",
-            tabsPosition: "top",
-            matchId: parentMatchId,
-        });
+            window.SIR("addWidget", ".sr-widget-1", "match.lmtPlus", {
+                streamToggle: "onPitchButton",
+                layout: "double",
+                detailedScoreboard: "disable",
+                tabsPosition: "top",
+                matchId: 50850415,
+            });
         }
-    };
+    }, []);
 
-    document.body.appendChild(script);
 
-    // Cleanup the script when the component unmounts
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
 
   return (
-    <div>
+    <div className="widgets">
       {/* Container for the widget */}
-      <div className="sr-widget-1"></div>
+      <div>
+        <div className="sr-widget sr-widget-1"></div>
+      </div>
     </div>
   );
 };
