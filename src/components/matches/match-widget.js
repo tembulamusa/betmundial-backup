03ef7b7734577e6b24f22794e70f1160796030d7
Moses tembula <jameswangabivan@gmail.com>
2025-01-29 17:06:15 +0300

    fix: fix unloading parentmatch id

--- src/components/matches/match-widget.js
+++ src/components/matches/match-widget.js
@@ -1,41 +1,49 @@
 import React, { useEffect } from "react";
 import '../../assets/css/theme.css';
 
-const MatchWidget = (props) => {
-    const {parentMatchId} = props;
-    useEffect(() => {
-    // Dynamically load the script
-    const script = document.createElement("script");
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = "https://widgets.sir.sportradar.com/507f748510ff83b1f73738bd3df69714/widgetloader";
-    script.setAttribute("n", "SIR");
 
-    script.onload = () => {
-        // Initialize the widget after the script has loaded
-        if (window.SIR) {
-        window.SIR("addWidget", ".sr-widget-1", "match.lmtPlus", {
-            streamToggle: "onPitchButton",
-            layout: "double",
-            detailedScoreboard: "disable",
-            tabsPosition: "top",
-            matchId: parentMatchId,
-        });
+
+const MatchWidget = (props) => {
+    const { parentMatchId } = props;
+     
+    const loadWidget = (parentMatchId) => {
+        if (!window.SIR) {
+            const script = document.createElement("script");
+            script.src = "https://widgets.sir.sportradar.com/507f748510ff83b1f73738bd3df69714/widgetloader";
+            script.async = true;
+            script.setAttribute("n", "SIR");
+            script.onload = () => {
+                window.SIR("addWidget", ".sr-widget-1", "match.lmtPlus", {
+                    streamToggle: "onPitchButton",
+                    layout: "double",
+                    detailedScoreboard: "disable",
+                    tabsPosition: "top",
+                    matchId: parentMatchId,
+                });
+            };
+            document.body.appendChild(script);
+        } else {
+            window.SIR("addWidget", ".sr-widget-1", "match.lmtPlus", {
+                streamToggle: "onPitchButton",
+                layout: "double",
+                detailedScoreboard: "disable",
+                tabsPosition: "top",
+                matchId: parentMatchId,
+            });
         }
     };
 
-    document.body.appendChild(script);
-
-    // Cleanup the script when the component unmounts
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+    useEffect(() => {
+        if(parentMatchId) {
+            loadWidget(parentMatchId);
+        }
+    }, [parentMatchId]);
 
   return (
-    <div>
-      {/* Container for the widget */}
-      <div className="sr-widget-1"></div>
+      <div class="widgets">
+        <div>
+          <div class="sr-widget sr-widget-1"></div>
+        </div>
     </div>
   );
 };
