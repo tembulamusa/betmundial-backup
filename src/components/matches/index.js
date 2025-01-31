99c8dd48c0c9256985e8e865731d7621ff20169f
Moses tembula <jameswangabivan@gmail.com>
2025-01-31 16:49:56 +0300

    feat:  fix stuff

--- src/components/matches/index.js
+++ src/components/matches/index.js
@@ -925,7 +925,7 @@ const MatchRow = (props) => {
                                 </span>
                                 : match?.start_time}
                         </span>
-                        <>ID: {!live && match?.game_id}</>
+                        {!live && <>ID:  && {match?.game_id} </>}
                     </div>
                 </div>
                 
