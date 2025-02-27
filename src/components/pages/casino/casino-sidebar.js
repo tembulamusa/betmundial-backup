3e092a60acfbb76a44db8b5ddf0fd68224e4feb4
Antony Njoroge <antonjoro2008@gmail.com>
2025-02-27 22:02:05 +0300

    some changes

--- src/components/pages/casino/casino-sidebar.js
+++ src/components/pages/casino/casino-sidebar.js
@@ -5,12 +5,12 @@ import { Link, useLocation, useNavigate, useSearchParams } from "react-router-do
 
 const CasinoSidebar = (props) => {
     const [state, dispatch] = useContext(Context);
-    const [categories, setCategories] = useState([]); 
+    const [categories, setCategories] = useState([]);
     const [providers, setProviders] = useState([]);
     const navigate = useNavigate();
-    const [searchParams, ] = useSearchParams();
+    const [searchParams,] = useSearchParams();
     const loc = useLocation();
-    
+
     const getSportImageIcon = (sport_name) => {
 
         let sport_image;
@@ -36,28 +36,28 @@ const CasinoSidebar = (props) => {
     useEffect(() => {
         let availableFilters = getFromLocalStorage("casinofilters");
         if (availableFilters) {
-            dispatch({type:"SET", key:"casinofilters", payload:availableFilters});
+            dispatch({ type: "SET", key: "casinofilters", payload: availableFilters });
         }
     }, [])
 
     const filterGames = (filterName, filterItem) => {
-        let payload = {filterType: "category", category: filterItem}
-        if(filterName == "category") {
-            if(filterItem?.name.toLowerCase() == "surecoin") {
-               navigate("/surecoin") 
+        let payload = { filterType: "category", category: filterItem }
+        if (filterName == "category") {
+            if (filterItem?.name.toLowerCase() == "surecoin") {
+                navigate("/surecoin")
             } else {
                 setLocalStorage("casinogamesfilter", payload);
-                dispatch({type:"SET", key:"casinogamesfilter", payload: payload});
+                dispatch({ type: "SET", key: "casinogamesfilter", payload: payload });
                 navigate(`/casino/categories/${filterItem?.name?.split(" ")?.join("")}`);
             }
         } else {
             removeItem("casinogamesfilter");
-            dispatch({type:"DEL", key:"casinogamesfilter"});
+            dispatch({ type: "DEL", key: "casinogamesfilter" });
             navigate(`/casino/${filterItem}`);
         }
     }
 
-    
+
 
     const CasinoCategories = (props) => {
         return (
@@ -65,26 +65,26 @@ const CasinoSidebar = (props) => {
                 <div className="casino-list-block menu-card rounded-lg  capitalize">
                     {/* <h1 className="my-2 mt-2 text-2xl font-[400] casino-class-header">Categories</h1> */}
                     <ul className="casino-sidebar-items">
-                        <li key={"all-" + 24} 
-                            className={`cursor-pointer menu-item capitalize` }
+                        <li key={"all-" + 24}
+                            className={`cursor-pointer menu-item capitalize`}
                             onClick={() => filterGames("all", "all")}>
-                            <img  src={getSportImageIcon('home')} className="casino-icon inline-block" alt=""/>{"All games"}
+                            <img src={getSportImageIcon('home')} className="casino-icon inline-block" alt="" />{"All games"}
                         </li>
-                        <li key={"popular-" + 53} 
-                            className={`cursor-pointer menu-item capitalize` }
+                        <li key={"popular-" + 53}
+                            className={`cursor-pointer menu-item capitalize`}
                             onClick={() => filterGames("popular", "popular")}>
-                            <img  src={getSportImageIcon('popular')} className="casino-icon inline-block" alt=""/>{"Popular"}
+                            <img src={getSportImageIcon('popular')} className="casino-icon inline-block" alt="" />{"Popular"}
                         </li>
-                        
+
                         {categories?.map((category, idx) => (
-                                <>
-                                <li to={`/casino?category=${category?.name?.toLowerCase()}&&id=${category?.id}`} key={"categories-" + idx} 
-                                    onClick={() =>filterGames('category', category) }
-                                    className={`${state?.casinogamesfilter?.category?.id == category?.id && 'active'} cursor-pointer menu-item block py-2 boder-b border-gray-100` }>
-                                    <img  src={getSportImageIcon(category.name)} className="casino-icon  inline-block" alt=""/>{category?.name}
+                            <>
+                                <li to={`/casino?category=${category?.name?.toLowerCase()}&&id=${category?.id}`} key={"categories-" + idx}
+                                    onClick={() => filterGames('category', category)}
+                                    className={`${state?.casinogamesfilter?.category?.id == category?.id && 'active'} cursor-pointer menu-item block py-2 boder-b border-gray-100`}>
+                                    <img src={getSportImageIcon(category.name)} className="casino-icon  inline-block" alt="" />{category?.name}
                                 </li>
-                                </>
-                            ))}
+                            </>
+                        ))}
                     </ul>
                 </div>
             </>
@@ -97,19 +97,19 @@ const CasinoSidebar = (props) => {
                 <div className="casino-list-block menu-card rounded-lg">
                     <h1 className="mb-2 text-2xl font-[400] casino-class-header">Providers</h1>
                     <ul className="casino-sidebar-items">
-                    {providers?.map((provider, idx) => (
-                        <Link to={provider?.name?.toLowerCase() == "surecoin" ? "/surecoin" : `/casino?provider=${provider?.name?.split(" ").join("-")}&&id=${provider?.id}`} 
-                            className={`${loc?.pathname?.includes(provider?.name) && 'active'} cursor-pointer menu-item block py-2` }>
-                            <img  src={getSportImageIcon(provider.name)} className="casino-icon inline-block" alt=""/>{provider?.name}
-                        </Link>))
-                    }
+                        {providers?.map((provider, idx) => (
+                            <Link to={provider?.name?.toLowerCase() == "surecoin" ? "/surecoin" : `/casino?provider=${provider?.name?.split(" ").join("-")}&&id=${provider?.id}`}
+                                className={`${loc?.pathname?.includes(provider?.name) && 'active'} cursor-pointer menu-item block py-2`}>
+                                <img src={getSportImageIcon(provider.name)} className="casino-icon inline-block" alt="" />{provider?.name}
+                            </Link>))
+                        }
                     </ul>
                 </div>
             </>
         )
     }
     return (
-        <div className="casino-sidebar ml-2">
+        <div className="casino-sidebar ml-2 mt-4">
             <h1 className="mb-2 bg-white pt-2 pb-3  text-4xl px-3 text-gray-600 font-[600] border-b border-gray-200">Casino</h1>
             {/* <Favorites /> */}
             <CasinoCategories />
